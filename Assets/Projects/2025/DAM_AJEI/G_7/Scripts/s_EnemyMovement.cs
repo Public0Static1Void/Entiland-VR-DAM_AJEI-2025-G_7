68abcdf3e8e560b8dbd91aacfa2585908debf5e9
Daniel Gonzalez Peralta <daniel.gonzalez@enti.cat>
2025-03-12 17:34:09 +0100

    Enemigos implementados (falta modelo)

--- Assets/Projects/2025/DAM_AJEI/G_7/Scripts/s_EnemyMovement.cs
+++ Assets/Projects/2025/DAM_AJEI/G_7/Scripts/s_EnemyMovement.cs
@@ -10,6 +10,9 @@ namespace EntilandVR.DosCinco.DAM_AJEI.G_Siete
     {
         private NavMeshAgent agent;
         public Transform target;
+
+        public List<Transform> targets;
+        private int current_target;
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
@@ -17,7 +20,26 @@ namespace EntilandVR.DosCinco.DAM_AJEI.G_Siete
 
         void Update()
         {
-            agent.SetDestination(target.transform.position);
+            if (Vector3.Distance(transform.position, targets[current_target].position) < 1)
+            {
+                current_target++;
+                if (current_target >= targets.Count)
+                {
+                    current_target = 0;
+                }
+            }
+            
+            if (current_target <= targets.Count)
+            {
+                agent.speed = 4;
+                agent.SetDestination(targets[current_target].position);
+            }
+
+            if (Vector3.Distance(transform.position, target.position) < 5)
+            {
+                agent.SetDestination(target.position);
+                agent.speed = 4;
+            }
         }
     }
 }
